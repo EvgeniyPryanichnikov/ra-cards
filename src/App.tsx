3fdd84a7541128993fb5eba8b49f196ae116632e
EvgeniyPryanichnikov <pryanichnikov.eugeny@yandex.ru>
2024-05-14 20:25:08 +0400

    init + task

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Cards from './components/Cards'
+
+function App() {
+  return (
+    <div className="container">
+        <Cards src={'https://i.ibb.co/qFV7CfT/react-svgrepo-com.png'}>
+          <h5 className="card-title">Card title</h5>
+
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+        </Cards>
+
+        <Cards src={'https://i.ibb.co/qFV7CfT/react-svgrepo-com.png'}>
+          <h5 className="card-title">Card title</h5>
+
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+        </Cards>
+    </div>
+  )
+}
+
+export default App
